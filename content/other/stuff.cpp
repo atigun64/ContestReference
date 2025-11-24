ab45fca90139b777b49e5e33bb9d4c4ef3fff5b8
Atilla Gün <atigun64@gmail.com>
2025-11-24 14:34:12 +0100

    Implement random number generator and clock display
    Added random number generation function and clock output.

--- content/other/stuff.cpp
+++ content/other/stuff.cpp
@@ -21,3 +21,9 @@ std::decimal::decimal128
 constexpr ll INF = 0x3FFF'FFFF'FFFF'FFFFll;
 // 1e9 < INF < Max_Value / 2
 constexpr int INF = 0x3FFF'FFFF;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rand(int l, int r){
+  return uniform_int_distribution<int>(l, r)(rng);
+}
+cerr << (double)clock() / CLOCKS_PER_SEC << " secs\n";
